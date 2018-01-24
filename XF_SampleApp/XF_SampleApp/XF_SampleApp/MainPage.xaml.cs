cd24146ca7b61ad72e96fe919c2a7fc843177c1e
Masayuki Fukuchi <masayuki.fukuchi@grapecity.com>
2018-01-24 14:17:53 +0900

    Initial Commit

---
+++ XF_SampleApp/XF_SampleApp/XF_SampleApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace XF_SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private int count = 0;
+        private int maxCount = 10;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            logo.Source = ImageSource.FromResource("XF_SampleApp.Resources.tagayas-logo-t_tp.png",Assembly.GetExecutingAssembly());
+            Button1.Clicked += Button1_Clicked;
+        }
+
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            count += 1;
+            Label1.Text = String.Format("こんにちは Xamarin.Forms! \n {0} 回目",count);
+            double progress = count / (double)maxCount;
+            ProgressBar1.ProgressTo(progress, 500, Easing.SpringIn);
+
+            if (count >= maxCount)
+            {
+                count = 0;
+            }
+        }
+    }
+}
